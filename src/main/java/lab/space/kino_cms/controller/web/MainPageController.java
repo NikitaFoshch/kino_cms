25e7fb2505e840fce34875f3683b38c8a64c10fb
WB_777 <antariuzz@gmail.com>
2023-03-06 19:23:35 +0200

    Some fixed

--- src/main/java/lab/space/kino_cms/controller/web/MainPageController.java
+++ src/main/java/lab/space/kino_cms/controller/web/MainPageController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("main")
+@RequestMapping({"/",""})
 public class MainPageController {
-    @GetMapping({"/",""})
+    @GetMapping
     public String showMainPage(){
         return "/web/pages/main-page/main-page";
     }
